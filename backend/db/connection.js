2e925895c4263fa443119ef5929f0c1eac110851
Evan <evant5252@gmail.com>
2024-12-30 14:24:13 -0800

    updated .env

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // MySQL connection
 const db = mysql.createConnection({
-    host: 'mysql',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'items_db'
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
